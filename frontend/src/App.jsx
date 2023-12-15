b99c9c5f8ac8bb68c330f2e719d4276f89c9464b
Irs444 <124651467+Irs444@users.noreply.github.com>
2023-12-15 14:34:19 +0530

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/user/Navbar'
 import Home from './components/user/Home'
 import Topics from './components/admin/Topics'
 import Questions from './components/admin/Questions'
-import Solution from './components/admin/Solution'
+
 import Solutions from './components/admin/Solutions'
 import Certificate from './components/admin/Certificate'
 
@@ -20,7 +20,7 @@ function App() {
      <Route path='/' element={<Home/>} />
      <Route path='/topics' element={<Topics/>} />
      <Route path='/questions' element={<Questions/>} />
-     <Route path='/solution' element={<Solution/>} />
+     
      <Route path='/solutions' element={<Solutions/>} />
      <Route path='/certificate' element={<Certificate/>} />
 
